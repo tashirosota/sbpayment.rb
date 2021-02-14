a6e9e7d6796b0e35a2dcdd2f3b7ece80439dbe38
Tashiro <g4160hc@gmail.com>
2021-02-14 11:17:02 +0900

    add spec to au commit request

---
+++ spec/requests/api/au/commit_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'securerandom'
+
+describe 'Au API behavior' do
+  before do
+    Sbpayment.configure do |x|
+      x.sandbox = true
+      x.merchant_id = '30132'
+      x.service_id  = '103'
+      x.basic_auth_user     = '30132103'
+      x.basic_auth_password = 'ed679e1c9f90c2ab96b25d5c580b58e25192eb5d'
+      x.hashkey             = 'ed679e1c9f90c2ab96b25d5c580b58e25192eb5d'
+      x.cipher_code         = 'ed679e1c9f90c2ab96b25d5c'
+      x.cipher_iv           = '580b58e2'
+    end
+  end
+  describe 'CommitRequest' do
+    around do |e|
+      VCR.use_cassette 'au-commit' do
+        e.run
+      end
+    end
+    let(:req) do
+      req = Sbpayment::API::Au::CommitRequest.new
+      req.tracking_id   = SecureRandom.hex
+      req.request_date  = Sbpayment::TimeUtil.format_current_time
+      req
+    end
+    describe '#attributes' do
+      it 'keys' do
+        expected_json_user_keys = %w(
+          merchant_id
+          service_id
+          tracking_id
+          request_date
+          limit_second
+          sps_hashcode
+        )
+        expect(req.attributes.keys).to eq expected_json_user_keys
+      end
+
+      describe '#perform' do
+        xcontext 'success' do
+          # couldn't implement because trackng_id is nil
+        end
+  
+        context 'failed' do
+          it do
+            res = req.perform
+            expect(res.error.class).to eq Sbpayment::API40103Error
+          end
+        end
+      end
+    end
+  end
+end
