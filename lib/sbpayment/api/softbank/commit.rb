e12e7f159a34f9a7e19272f07d21f7f02e05fcbb
Tashiro <g4160hc@gmail.com>
2021-02-28 05:01:11 +0900

    Sbpayment::API::Softbank::CommitRequest is deprecated

--- lib/sbpayment/api/softbank/commit.rb
+++ lib/sbpayment/api/softbank/commit.rb
@@ -4,11 +4,24 @@ require_relative '../../response'
 module Sbpayment
   module API
     module Softbank
+      class PayOptionManage
+        include ParameterDefinition
+
+        tag 'pay_option_manage'
+        key :amount
+      end
+
       class CommitRequest < Request
+        def initialize(*arg, &blk)
+          warn 'Sbpayment::API::Softbank::CommitRequest is deprecated, should use Sbpayment::API::Softbank::SalesRequest'
+          super(*arg, &blk)
+        end
+ 
         tag 'sps-api-request', id: 'ST02-00201-405'
         key :merchant_id, default: -> { Sbpayment.config.merchant_id }
         key :service_id,  default: -> { Sbpayment.config.service_id }
         key :tracking_id
+        key :pay_option_manage, class: PayOptionManage
         key :request_date, default: -> { TimeUtil.format_current_time }
         key :limit_second
         key :sps_hashcode
@@ -19,4 +32,3 @@ module Sbpayment
     end
   end
 end
-
